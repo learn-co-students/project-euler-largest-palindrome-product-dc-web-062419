e6e90b7ae3986b882d201e9eebbfb2cfb4dd871b
ruggz13 <nicolasruggieri7@gmail.com>
2019-09-26 10:33:23 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,53 @@
-# Implement your procedural solution here!
+# # Implement your procedural solution here!
+# def largest_palindrome_product(num1, num2)
+#     reverseNum = 0
+#     product = num1*num2
+#     reverseNum = product.to_s().reverse()
+#     if reverseNum == product.to_s()
+#         product
+#     end
+# end
+
+def largest_palindrome_product()
+    max_product = 0
+    for i in (100..999)
+        for j in (100..999)
+            product = i * j
+            if (product.to_s == product.to_s.reverse && product > max_product)
+                max_product = product
+            end
+        end
+    end
+    return max_product
+end
+
+# def solution 
+
+#     r = 0;
+
+#   for p = 990; p > 99; p-= 11
+
+#     for q = 999; q > 99; q--
+
+#       t = p * q;
+
+#         if r < t && isPalindrome(t)
+#             r = t;
+#             break;
+#         elsif t < r
+#             break;
+#         end
+#   return r;
+# end
+
+# def isPalindrome(x)
+
+#     n = 0;
+#     m = x;
+
+#     while x > 0
+#         n = n * 10 + x % 10;
+#         x = x / 10 | 0;
+#     end
+#     return n === m;
+# end
